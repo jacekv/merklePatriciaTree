986b709cb3bb7a85394252893029c1ac9738e329
JacekV <jaca347@gmail.com>
2019-05-22 09:38:20 +0200

    Added secureTrieTest as test case

--- mpt/utils.py
+++ mpt/utils.py
@@ -1,8 +1,10 @@
 from sha3 import keccak_256
 
+
 def sha3(seed):
-    hash = keccak_256(seed).digest()
-    return hash
+    #hash = keccak_256(seed).digest()
+    return keccak_256(seed).digest()
+
 
 def keccak_hash(seed):
     return keccak_256(seed).digest()
